bdc5515e4111c3d9ae51bf0563853cbe6d7c4de5
beebee1 <dai.linh@northeastern.edu>
2023-03-30 19:41:27 -0700

    add banner

--- src/farmers-home/index.js
+++ src/farmers-home/index.js
@@ -5,6 +5,11 @@ const FarmersHomeComponent = () => {
   return (
       <>
         <h1>farmers home</h1>
+        <div className="position-relative mb-2">
+          <img src="/images/home-banner.png" className="w-100 mb-3"/>
+          <h1 className="position-absolute wd-picture-letter text-warning">
+            Welcome to your local Farmer's Market</h1>
+        </div>
         <ProductList/>
       </>
   );
