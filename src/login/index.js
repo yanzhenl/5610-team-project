90c3ccaf0dfa37b81ef4518866f0e1cd57f602db
evelynzz <zhao.yue3@northeastern.edu>
2023-04-02 00:10:24 -0700

    Added login/signup page

--- src/login/index.js
+++ src/login/index.js
@@ -1,24 +1,55 @@
-import React from "react";
+import React, { useState } from "react";
+import "./index.css";
+import {Link} from "react-router-dom";
+import FloatingLabel from 'react-bootstrap/FloatingLabel';
+import Form from 'react-bootstrap/Form';
 
 function Login() {
+    const [username, setUsername] = useState("");
+    const [password, setPassword] = useState("");
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        // Handle login logic here (e.g., call an API, update the state, etc.)
+    };
+
     return (
-        <div className="login">
-            <div className="login-container">
-                <h2>Login</h2>
-                <form>
-                    <div className="form-group">
-                        <label htmlFor="username">Username</label>
-                        <input type="email" id="email"/>
-                    </div>
-                    <div className="form-group">
-                        <label htmlFor="password">Password</label>
-                        <input type="password" id="password"/>
-                    </div>
-                    <button type="submit">Login</button>
-                </form>
+        <div className="wd-login-background">
+            <div className="wd-login-page">
+                <div className="wd-login-container">
+                    <h2>Welcome back</h2>
+                    <form onSubmit={handleSubmit}>
+                        <div className="mt-4 pt-2 ">
+                            <FloatingLabel controlId="floatingName" label="Username" className="mb-4"
+                                           htmlFor="username">
+                                <Form.Control type="username" placeholder="Username"
+                                              value={username}
+                                              onChange={(e) => setUsername(e.target.value)}
+                                              required/>
+                            </FloatingLabel>
+                        </div>
+                        <div className="mb-3 pb-1">
+                            <FloatingLabel controlId="floatingPassword" label="Password"
+                                           htmlFor="password">
+                                <Form.Control type="password" placeholder="Password"
+                                              value={password}
+                                              onChange={(e) => setPassword(e.target.value)}
+                                              required/>
+                            </FloatingLabel>
+                        </div>
+
+                        <button className="wd-login-button" type="submit">Log in</button>
+
+                        <div className="mt-4">
+                            Don't have an account?
+                            <Link to="/signup">
+                                <span> Sign up now</span>
+                            </Link>
+                            .
+                        </div>
+                    </form>
+                </div>
             </div>
-
-
         </div>
     );
 }
--- src/sign-up/index.js
+++ src/sign-up/index.js
@@ -1,9 +1,67 @@
-import React from "react";
+import React, { useState } from "react";
+import "../login/index.css";
+import {Link} from "react-router-dom";
+import FloatingLabel from 'react-bootstrap/FloatingLabel';
+import Form from 'react-bootstrap/Form';
 
 function SignUp() {
+    const [username, setUsername] = useState("");
+    const [password, setPassword] = useState("");
+    const [confirmPassword, setConfirmPassword] = useState("");
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        // Handle login logic here (e.g., call an API, update the state, etc.)
+    };
+
     return (
-        <div>
-            <h1>Sign Up</h1>
+        <div className="wd-login-background">
+            <div className="wd-login-page">
+                <div className="wd-login-container">
+                    <h2>Create account</h2>
+                    <form onSubmit={handleSubmit}>
+                        <div className="mt-4 pt-2 ">
+                            <FloatingLabel controlId="floatingName" label="Username" className="mb-4"
+                                           htmlFor="username">
+                                <Form.Control type="username" placeholder="Username"
+                                              value={username}
+                                              onChange={(e) => setUsername(e.target.value)}
+                                              required/>
+                            </FloatingLabel>
+                        </div>
+                        <div>
+                            <FloatingLabel controlId="floatingPassword" label="Password" className="mb-4"
+                                           htmlFor="password">
+                                <Form.Control type="password" placeholder="Password"
+                                              value={password}
+                                              onChange={(e) => setPassword(e.target.value)}
+                                              required/>
+                            </FloatingLabel>
+                        </div>
+
+                        <div className="mb-4 pb-1">
+                            <FloatingLabel controlId="floatingConfirmPassword" label="Confirm password"
+                                           htmlFor="confirmPassword">
+                                <Form.Control type="confirmPassword" placeholder="confirmPassword"
+                                              value={confirmPassword}
+                                              onChange={(e) => setConfirmPassword(e.target.value)}
+                                              required/>
+                            </FloatingLabel>
+                        </div>
+
+
+                        <button className="wd-login-button" type="submit">Sign up</button>
+
+                        <div className="mt-4">
+                            Already have an account?
+                            <Link to="/login">
+                                <span> Log in</span>
+                            </Link>
+                            .
+                        </div>
+                    </form>
+                </div>
+            </div>
         </div>
     );
 }
