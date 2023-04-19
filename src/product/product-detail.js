e0f1dbbbbfe327865f6b9f6c2b9c0cc046f96d1a
beebee1 <dai.linh@northeastern.edu>
2023-04-19 01:33:46 -0700

    change farmers-home and add api

--- src/product/product-detail.js
+++ src/product/product-detail.js
@@ -1,20 +1,35 @@
-import React from "react";
-import {useLocation, useNavigate, useParams} from "react-router";
+import React, {useEffect, useState} from "react";
+import {useParams} from "react-router";
 import {Link} from "react-router-dom";
 
 const ProductDetail = () => {
   const { id } = useParams();
-  const location = useLocation();
-  const navigate = useNavigate();
-  const product = location.state.product
-  if (!product) {
-    // handle the case where the state object is null or undefined
-    return <div>Product not found</div>;
+  const [product, setProduct] = useState({});
+  const url = `https://weee-grocery-api-sayweee-com-browsing-searching-details.p.rapidapi.com/details?product_id=${id}&zipcode=77494`;
+  const options = {
+    method: 'GET',
+    headers: {
+      'X-RapidAPI-Key': '56af78389dmsh0fe658990c44224p1aa19djsn2be0a4206a11',
+      'X-RapidAPI-Host': 'weee-grocery-api-sayweee-com-browsing-searching-details.p.rapidapi.com'
+    }
+  };
+
+  const fetchme = () => {
+    fetch(url, options)
+    .then((response) => response.json())
+    .then((res) => {
+      console.log(res.data.detail);
+      setProduct(res.data.detail.product);
+    })
+    .catch((err) => {
+      console.log(err.message);
+    });
   }
 
-  function handleClick() {
-    navigate('/farmers-home/edit-detail', {state: {product}});
-  }
+  useEffect(() => {
+    fetchme()
+  }, []);
+
   return (
       <div style={{width : 500}}>
         <button  className="btn rounded-pill text-dark pt-1 float-end">
@@ -24,17 +39,21 @@ const ProductDetail = () => {
         </button>
         <h1>Product Detail</h1>
         <div className={"container"}>
-          <img src={`../images/${product.image}`} alt={product.id} height={400} className="rounded-3"/>
+          <img src={`${product.img}`} alt={id} height={400} className="rounded-3"/>
           <span className="position-relative" style={{left: 460, bottom: 400}}>
             <h3 className="float-end">${product.price}/lb</h3>
-            <h3>{product.productName}</h3>
-            {product.descriptions}<br/>
-            {product.availability === true && "In Stock"}
-            {product.availability === false && "Out of Stock"}
-            <button onClick={handleClick}
-                className="btn rounded-pill border float-end">
-              Edit Detail
+            <h3>{product.name}</h3>
+            <button type="button" className="float-end btn btn-outline-primary btn-sm"
+                    style={{ borderRadius: '25px', padding: '0.15rem 0.75rem' }}>
+              <i className="bi bi-cart-plus"></i>
             </button>
+            {/*{product.descriptions}<br/>*/}
+            {/*{product.availability === true && "In Stock"}*/}
+            {/*{product.availability === false && "Out of Stock"}*/}
+            {/*<button onClick={handleClick}*/}
+            {/*    className="btn rounded-pill border float-end">*/}
+            {/*  Edit Detail*/}
+            {/*</button>*/}
           </span>
         </div>
       </div>
--- src/product/product-item.js
+++ src/product/product-item.js
@@ -15,34 +15,51 @@ const ProductItem = (
       }
     }
 ) => {
-  console.log(product.images.sizes)
+  // console.log(product.images.sizes)
   // const navigate = useNavigate();
   // function handleImageClick() {
   //   navigate('/farmers-home/product-detail', {state: {product}});
   // }
   return (
-      <div className="border rounded-2">
+      <div>
         {/*<Link to='/farmers-home/product-detail' state={product} style={{textDecoration : "none"}}>*/}
         {/*  <img src={`images/${product.image}`} alt={product.id} height={200} className="rounded-top w-100"/>*/}
         {/*</Link>*/}
-        <div>
-          <img src={`${product.images[1]}`} alt={product.productId} height={200} className="rounded-top w-100"/>
-        </div>
-        <div>
-          <span className="ps-2 fw-bold">
-            {product.name}
-          </span>
-        </div>
-        <div>
-          <button className="btn rounded-pill border float-end">
-            add
-          </button>
-          {product.availability === true && "In Stock"}
-          {product.availability === false && "Out of Stock"}
-          <br/>
-          ${product.price}/lb
-        </div>
         {/*<div>*/}
+        {/*  <img src={`${product.square_img_url}`} alt={product.id} height={200} className="rounded-top w-100"/>*/}
+        {/*</div>*/}
+        {/*<div>*/}
+        {/*  <span className="ps-2 fw-bold">*/}
+        {/*    {product.name}*/}
+        {/*  </span>*/}
+        {/*</div>*/}
+        {/*<div>*/}
+        {/*  <button className="btn rounded-pill border float-end">*/}
+        {/*    add*/}
+        {/*  </button>*/}
+        {/*  {product.availability === true && "In Stock"}*/}
+        {/*  {product.availability === false && "Out of Stock"}*/}
+        {/*  <br/>*/}
+        {/*  ${product.price}/lb*/}
+        {/*</div>*/}
+        {/*<div>*/}
+        <div className="mt-2 ps-2">
+          <img height={200} className="w-100 rounded-4"
+               src={`${product.img}`} alt={product.id}/>
+        </div>
+        <div className="mt-2 ps-2 mb-2 wd-truncate-title"
+             style={{fontSize: "0.9rem", color: "dimgray"}}>
+          {product.name}
+        </div>
+        <span className="mt-1 ms-2 fw-bold">
+            ${product.price}/lb
+        </span>
+        <span>
+            <button type="button" className="float-end btn btn-outline-primary btn-sm"
+                    style={{ borderRadius: '25px', padding: '0.15rem 0.75rem' }}>
+              <i className="bi bi-cart-plus"></i>
+            </button>
+        </span>
         {/*  /!*<button className="btn rounded-pill border">*!/*/}
         {/*  /!*  <Link to="/product/detail" style={{textDecoration : "none"}}>*!/*/}
         {/*  /!*    Product Detail*!/*/}
