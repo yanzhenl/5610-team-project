2c938b24a18fb2382404649fd9ff24162aaa79b0
evelynzz <zhao.yue3@northeastern.edu>
2023-04-18 22:01:27 -0700

    Apply cookies

--- src/services/users/users-service.js
+++ src/services/users/users-service.js
@@ -1,14 +1,17 @@
 import axios from "axios";
 const USERS_API_URL = "http://localhost:8000/api/users";
 
+const api = axios.create({
+    withCredentials: true,
+});
+
 export const findAllUsers = async () => {
     const response = await axios.get(USERS_API_URL);
     return response.data;
 };
 
 export const findUserById = async (id) => {
-    // return axios.get(`${USERS_API_URL}/${id}`).then((response) => response.data);
-    const response = await axios.get(`${USERS_API_URL}/${id}`);
+    const response = await axios.get(`${USERS_API_URL}/userId/${id}`);
     return response.data;
 };
 
@@ -16,9 +19,8 @@ export const createUser = (user) => {
     return axios.post(USERS_API_URL, user);
 };
 
-export const updateUser = async (newUser) => {
-    const response = await axios.put(`${USERS_API_URL}/${newUser._id}`, newUser);
-    return response.data;
+export const updateUser = (newUser) => {
+    return api.put(`${USERS_API_URL}/${newUser._id}`, newUser);
 };
 
 export const deleteUser = (id) => {
@@ -26,19 +28,17 @@ export const deleteUser = (id) => {
 };
 
 export const login = (user) => {
-    return axios.post(`${USERS_API_URL}/login`, user);
+    return api.post(`${USERS_API_URL}/login`, user);
 };
 
 export const logout = () => {
-    return axios.post(`${USERS_API_URL}/logout`);
+    return api.post(`${USERS_API_URL}/logout`);
 };
 
 export const register = (user) => {
-    return axios.post(`${USERS_API_URL}/register`, user);
+    return api.post(`${USERS_API_URL}/register`, user);
 };
 
-export const profile = async () => {
-    return await axios.get(`${USERS_API_URL}/profile`);
-    // console.log(response.data);
-    // return response.data;
+export const profile = () => {
+    return api.get(`${USERS_API_URL}/profile`);
 };
