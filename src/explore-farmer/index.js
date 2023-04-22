f57045076406dcece425fe4801521ba7a88f165e
yanzhenl <li.yanzhe@northeastern.edu>
2023-04-21 17:25:29 -0700

    change button for explore farmer page

--- src/explore-farmer/index.js
+++ src/explore-farmer/index.js
@@ -36,32 +36,30 @@ function FarmerList() {
                 <div className="row">
                     {farmers.map((farmer) => (
                         farmer.opened &&(
-                        <Link to={`/farmers/${farmer.businessName}/${farmer.zipcode}`}
-                              style={{ textDecoration: 'none', color: 'black' }}>
-                            <li className="list-group-item mt-3 mb-2 d-flex align-items-center farmer-container">
-                                <img width={50} className="rounded-pill float-start inner-content ms-3"
-                                     src={`/images/${farmer.profilePicture}`} />
-                                <div className="col-11 ms-2 float-start inner-content d-flex justify-content-between">
-                                    <div>
+                            <Link to={`/farmers/${farmer.businessName}/${farmer.zipcode}`}
+                                  style={{ textDecoration: 'none', color: 'black' }}>
+                                <li className="list-group-item mt-3 mb-2 d-flex align-items-center farmer-container">
+                                    <img width={50} className="rounded-pill float-start inner-content ms-3"
+                                         src={`/images/${farmer.profilePicture}`} />
+                                    <div className="col-11 ms-2 float-start inner-content d-flex justify-content-between">
+                                        <div>
                                         <span className="ps-2 fw-bold">
                                             {farmer.businessName}
                                         </span>
-                                        <i className="fa-solid fa-circle-check ps-1"></i>
-                                        <span className="ps-2" style={{color: "dimgray"}}>
+                                            <i className="fa-solid fa-circle-check ps-1"></i>
+                                            <span className="ps-2" style={{color: "dimgray"}}>
                                             {farmer.handle}
                                         </span>
-                                        <div className="ps-2 inner-content">
-                                            {farmer.bio}
+                                            <div className="ps-2 inner-content">
+                                                {farmer.bio}
+                                            </div>
+                                        </div>
+                                        <div className="d-flex flex-column justify-content-center">
+                                            <button className="btn btn-success rounded-pill me-1">Enter</button>
                                         </div>
                                     </div>
-                                    {currentUser &&(
-                                    <div className="d-flex flex-column justify-content-center">
-                                        <button className="btn btn-success rounded-pill me-1">Follow</button>
-                                    </div>
-                                    )}
-                                </div>
-                            </li>
-                        </Link>
+                                </li>
+                            </Link>
                         )
                     ))}
                 </div>
