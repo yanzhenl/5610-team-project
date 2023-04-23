c2d3c52e3c46f6785bf88401c75d942fe22489f2
beebee1 <dai.linh@northeastern.edu>
2023-04-22 21:47:20 -0700

    set a condition for the url in product list

--- src/product/index.js
+++ src/product/index.js
@@ -10,6 +10,9 @@ const ProductList = () => {
   const [products,setProducts] = useState([]);
 
   const  url = `https://weee-grocery-api-sayweee-com-browsing-searching-details.p.rapidapi.com/search?zipcode=98105&keyword=${category_name}&limit=20&ffset=0`;
+  if (category_name == "nothing") {
+    url = `https://weee-grocery-api-sayweee-com-browsing-searching-details.p.rapidapi.com/search?zipcode=98105&keyword=${category_name}&limit=0&ffset=0`;
+  }
 
   const options = {
     method: 'GET',
